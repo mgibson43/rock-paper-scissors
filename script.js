dbd14d24e2d929c9f8af86de479c5a964bea523b
mgibson <matthew.jgibson43@gmail.com>
2022-10-17 10:24:26 -0400

    Feat: game function, playerSelection changed to prompt

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const win = ['rockscissors', 'paperrock', 'scissorspaper'];
+let playerScore = 0;
+let computerScore = 0;
 
 const getComputerChoice = function() {
   const num = Math.trunc((Math.random() * 3) + 1);
@@ -12,11 +14,29 @@ const getComputerChoice = function() {
 const playRound = function(playerSelection,computerSelection) {
   if (playerSelection === computerSelection) return `Draw, ${computerSelection} ties with ${playerSelection}`;
 
-  if (win.includes(playerSelection + computerSelection)) return `Player wins! ${playerSelection} beats ${computerSelection}.`;
+  if (win.includes(playerSelection + computerSelection)) {
+    playerScore++;
+    return `Player wins! ${playerSelection} beats ${computerSelection}.`;
+  }
 
+  computerScore++;
   return `You lose! ${computerSelection} beats ${playerSelection}.`;
 };
 
-const playerSelection = "rock".trim().toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(computerSelection, playerSelection));
+const game = function() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt('Rock, Paper, or Scissors?').trim().toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (playerScore > computerScore) console.log("Player wins!");
+  else if (computerScore > playerScore) console.log("Computer wins!");
+  else console.log("Tie game!")
+  playerScore = computerScore = 0;
+}
+
+game();
+
+
+
+
