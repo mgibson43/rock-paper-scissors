fc847713db21b56cfc85096053e41c2f5f1e506a
mgibson <matthew.jgibson43@gmail.com>
2022-10-17 10:13:54 -0400

    Feat: playRound function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const win = ['rockscissors', 'paperrock', 'scissorspaper'];
+
 const getComputerChoice = function() {
   const num = Math.trunc((Math.random() * 3) + 1);
   if (num === 1) return "rock";
@@ -7,4 +9,14 @@ const getComputerChoice = function() {
   if (num === 3) return "scissors"
 }
 
+const playRound = function(playerSelection,computerSelection) {
+  if (playerSelection === computerSelection) return `Draw, ${computerSelection} ties with ${playerSelection}`;
+
+  if (win.includes(playerSelection + computerSelection)) return `Player wins! ${playerSelection} beats ${computerSelection}.`;
+
+  return `You lose! ${computerSelection} beats ${playerSelection}.`;
+};
+
+const playerSelection = "rock".trim().toLowerCase();
 const computerSelection = getComputerChoice();
+console.log(playRound(computerSelection, playerSelection));
